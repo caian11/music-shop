f9c831d46a26c4b0a8723b8761851106342ba4c1
caian <caian@eficienciafiscal.com.br>
2025-07-14 16:54:43 -0400

    update

--- src/migrations/1752506938252-CreateEnderecoTable.ts
+++ src/migrations/1752506938252-CreateEnderecoTable.ts
@@ -3,6 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class CreateEnderecoTable1752506938252 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
